ecedef2a2ebc607725bd8425f16567c359522f1f
calvambro <83657591+calvambro@users.noreply.github.com>
2025-02-02 15:13:49 +0700

    initial upload

---
+++ Sistem_Pelacakan.ino
@@ -0,0 +1,66 @@
+// initialize switch actuator
+const int RPWM = 8;
+const int LPWM = 9;
+const int R_EN = 10;
+const int L_EN = 11;
+
+// initialize sensor LDR
+const int sensorLDRKa = A2;
+const int sensorLDRKi = A3;
+
+// initialize sensor value 
+int valKa, valKi, Speed;
+
+void setup() {
+
+  // configure motor driver as an output
+  pinMode(RPWM, OUTPUT);
+  pinMode(LPWM, OUTPUT);
+  pinMode(R_EN, OUTPUT);
+  pinMode(L_EN, OUTPUT);
+
+  // configure sensor LDR as an input
+  pinMode(sensorLDRKa, INPUT);
+  pinMode(sensorLDRKi, INPUT);
+  
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Speed = 100;
+  valKa = analogRead(sensorLDRKa);
+  valKi = analogRead(sensorLDRKi);
+  analogWrite(R_EN, 500);
+  analogWrite(L_EN, 500);
+
+  Serial.println("Timur: " + String(valKa) + "  Barat: " + String(valKi));
+  delay(50);
+
+  if(valKa <100 && valKi < 100){
+    if(valKa - valKi < -10){
+      analogWrite(RPWM, 0);
+      analogWrite(LPWM, Speed);
+      Serial.println("Turun");
+    } else if (valKa - valKi > 10){
+      analogWrite(LPWM, 0);
+      analogWrite(RPWM, Speed);
+      Serial.println("Naik");
+    } else{
+      analogWrite(RPWM, 0);
+      analogWrite(LPWM, 0);
+      delay(600000);
+    }
+  }else if (valKa >300 && valKi > 300){
+      analogWrite(RPWM, 0);
+      analogWrite(LPWM, Speed);
+      Serial.println("resetting");
+      delay(2000);
+  } else{
+      analogWrite(RPWM, 0);
+      analogWrite(LPWM, 0);
+  }
+  
+  delay(1000); // 10 Seconds
+
+}
